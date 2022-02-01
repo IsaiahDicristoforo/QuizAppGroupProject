8d5e4104dc847fba2fda3ffe7c79ccfdb6e3ae15
Isaiah Dicristoforo <48969369+IsaiahDicristoforo@users.noreply.github.com>
2022-02-01 17:26:14 -0500

    More word grid and layout improvements

--- src/main/resources/static/js/gameScreen.js
+++ src/main/resources/static/js/gameScreen.js
@@ -1,6 +1,6 @@
 
 let row = 1;
-let column = 1;
+let column = 0;
 
 $(document).ready(function(){
 
@@ -8,36 +8,62 @@ $(document).ready(function(){
 
     $(document).on('keydown', (event) => {
 
-        if(event.key === "Backspace"){
-            $("#row" + row + "column" + column).empty()
-            column--;
-
-        }else{
-            $("#row" + row + "column" + column).text(event.key.toUpperCase())
-
-            anime({
-                targets:  $("#row" + row + "column" + column).get(),
-                scale: ["100%", "120%"],
-                direction: "alternate",
-                easing: 'easeInOutSine',
-                duration: 250
-
-            })
-            column++;
-
-
-        }
+        handleLetterEntered(event);
 
     })
 
 
 })
 
+function handleLetterEntered(event){
+
+    if(event.key === "Enter"){
+        row++;
+        column = 0;
+
+        let targetsArray = ["#row1column1", '#row1column2', '#row1column3', '#row1column4', '#row1column5']
+
+        let colors =  ["#d1ccd8", '#409E51', '#D9D426' ]
+
+        let color = Math.floor(Math.random() * 11);
+
+        anime({
+            targets: targetsArray,
+            direction: "normal",
+            easing: 'easeInOutSine',
+            duration: 500,
+            rotate: '1turn'
+        })
+    }
+    else if(event.key === "Backspace"){
+
+        $("#row" + row + "column" + column).empty()
+        column--;
+
+    }else if(event.keyCode >= 60 && event.keyCode <= 90){ //Checking to see if the user enters a letter.
+
+        column++;
+
+        $("#row" + row + "column" + column).text(event.key.toUpperCase())
+
+        anime({
+            targets:  $("#row" + row + "column" + column).get(),
+            scale: ["100%", "120%"],
+            border: ["1px solid white", "1px solid #1bba3d"],
+            direction: "alternate",
+            easing: 'easeInOutSine',
+            duration: 250
+
+        })
+    }
 
 
+
+
+}
 function createGrid(wordLength, totalGuesses){
-
-
+    row = 1;
+    column = 0;
     $("#wordleGridContainer").empty();
     $("#wordleGridContainer").css("grid-template-columns", "repeat(" + wordLength + ", 0.08fr")
 
@@ -47,13 +73,17 @@ function createGrid(wordLength, totalGuesses){
             let newElement = document.createElement("div");
             newElement.innerHTML = "&nbsp"
             newElement.classList.add("letter");
+
+            console.log("row" + i + "column" + j);
             newElement.id = ("row" + i + "column" + j);
 
 
             anime({
                 targets: newElement,
                 opacity: ['0','1'],
-                duration: 2000,
+                duration: 700,
+                scale: ["0%", "100%"],
+                easing: 'easeInOutSine'
             })
 
             document.getElementById("wordleGridContainer").appendChild(newElement)
