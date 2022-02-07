e0b5870c1b5e806084ada5b92eaf428e63114051
Isaiah Dicristoforo <48969369+IsaiahDicristoforo@users.noreply.github.com>
2022-02-07 11:02:48 -0500

    Wrapping up core registration and login functionality

---
+++ src/main/java/com/quizapp/enterprise/models/User.java
@@ -0,0 +1,32 @@
+package com.quizapp.enterprise.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userID;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    private String lastName;
+
+    @NotNull
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private int totalPoints;
+
+}
