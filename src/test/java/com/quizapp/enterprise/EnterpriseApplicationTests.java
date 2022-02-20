a1124ac0f0b96f6b0012acfcae0cde41a9e686c5
MSG17 <37581557+MSG17@users.noreply.github.com>
2022-02-20 17:58:26 -0500

    Service unit test implemented, won't currently pass but will look at service class to understand it better

--- src/test/java/com/quizapp/enterprise/EnterpriseApplicationTests.java
+++ src/test/java/com/quizapp/enterprise/EnterpriseApplicationTests.java
@@ -1,10 +1,15 @@
 package com.quizapp.enterprise;
 
 import com.quizapp.enterprise.models.game.Game;
+import com.quizapp.enterprise.services.GameService;
+import com.quizapp.enterprise.services.IGameService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class EnterpriseApplicationTests {
@@ -15,6 +20,7 @@ class EnterpriseApplicationTests {
 
     /**
      * Validate that the DTO properties can be set and retrieved. (Based on other unit tests in class)
+     * @author Mahesh Gowda
      */
     @Test
     void verifyGameProperties() {
@@ -24,4 +30,30 @@ class EnterpriseApplicationTests {
         game.setGameCode(gameCode);
         assertEquals(gameCode, game.getGameCode());
     }
+
+    /**
+     * Validate that the GameService can save and return games. (based on individual assignment)
+     * @author Mahesh Gowda
+     */
+    @Test
+    void verifyAddAndRemoveGames() {
+        String gameCode =  "FIRST";
+
+        Game game = new Game();
+        game.setGameCode(gameCode);
+
+        List<Game> games = GameService.getAllGames();
+        boolean gamePresent = false;
+        for (Game g : games) {
+            if (g.getGameCode().equals(gameCode)) {
+                gamePresent = true;
+                break;
+            }
+        }
+
+        assertTrue(gamePresent);
+
+
+    }
+
 }
