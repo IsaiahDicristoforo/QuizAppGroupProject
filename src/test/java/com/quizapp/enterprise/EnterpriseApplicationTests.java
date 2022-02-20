dec0b23153c830db800f4da268436451146b81c8
MSG17 <37581557+MSG17@users.noreply.github.com>
2022-02-19 20:20:20 -0500

    Started implementing unit tests

--- src/test/java/com/quizapp/enterprise/EnterpriseApplicationTests.java
+++ src/test/java/com/quizapp/enterprise/EnterpriseApplicationTests.java
@@ -1,7 +1,10 @@
 package com.quizapp.enterprise;
 
+import com.quizapp.enterprise.models.game.Game;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class EnterpriseApplicationTests {
@@ -10,4 +13,15 @@ class EnterpriseApplicationTests {
     void contextLoads() {
     }
 
+    /**
+     * Validate that the DTO properties can be set and retrieved. (Based on other unit tests in class)
+     */
+    @Test
+    void verifyGameProperties() {
+        String gameCode =  "TEST";
+
+        Game game = new Game();
+        game.setGameCode(gameCode);
+        assertEquals(gameCode, game.getGameCode());
+    }
 }
