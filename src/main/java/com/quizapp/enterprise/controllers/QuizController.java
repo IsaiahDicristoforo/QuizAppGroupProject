2631376547d90dd8d6a812b91206ca7b1fe5bcec
Isaiah Dicristoforo <48969369+IsaiahDicristoforo@users.noreply.github.com>
2022-02-28 16:31:15 -0500

    More ui backend wireup

--- src/main/java/com/quizapp/enterprise/controllers/QuizController.java
+++ src/main/java/com/quizapp/enterprise/controllers/QuizController.java
@@ -12,11 +12,13 @@ import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 public class QuizController {
@@ -24,10 +26,12 @@ public class QuizController {
     @Autowired
     private IQuizService quizService;
 
+
     @RequestMapping("/quizzes")
     public String userQuizes(Model model, Principal principal){
 
         model.addAttribute("username",principal.getName());
+        model.addAttribute("quizzes", quizService.getAllQuizzes(principal.getName()));
         return "quiz_list";
     }
 
@@ -40,6 +44,12 @@ public class QuizController {
     @ResponseBody
     public Quiz createNewQuiz(@RequestBody Quiz quizToAdd, Authentication authenticationDetails){
         return quizService.createQuiz(quizToAdd, authenticationDetails.getName());
+    }
+
+    @GetMapping("/quiz/{id}")
+    @ResponseBody
+    public Quiz getQuizById(@PathVariable("id") int id) throws Exception {
+        return quizService.getQuiz(id);
     }
 
 }
--- src/main/java/com/quizapp/enterprise/services/IQuizService.java
+++ src/main/java/com/quizapp/enterprise/services/IQuizService.java
@@ -2,7 +2,12 @@ package com.quizapp.enterprise.services;
 
 import com.quizapp.enterprise.models.Quiz;
 
+import java.util.ArrayList;
+
 public interface IQuizService {
 
     Quiz createQuiz(Quiz quizToAdd, String username);
+    Quiz getQuiz(int quizId) throws Exception;
+
+    ArrayList<Quiz> getAllQuizzes(String username);
 }
